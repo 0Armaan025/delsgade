eaa62c5b5e6bd7c50aea5dadde40f92a27c00355
Armaan <armaan33000@gmail.com>
2024-12-24 13:14:57 +0530

    respo navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,10 +6,10 @@ type Props = {};
 const Navbar = (props: Props) => {
   return (
     <>
-      <div className="navbar sticky top-4 mx-20 rounded-lg cursor-pointer transition-all hover:scale-105 bg-white/5 my-2 backdrop-blur-md shadow-md blur-[0.2px] shadow-black p-4 text-center">
+      <div className="navbar sticky top-4 mx-4 sm:mx-8 md:mx-16 lg:mx-20 rounded-lg cursor-pointer transition-all hover:scale-105 bg-white/5 my-2 backdrop-blur-md shadow-md blur-[0.2px] shadow-black p-4 text-center">
         <h1
-          className="text-3xl font-bold text-white font-poppins"
-          style={{ fontFamily: "Poppins,sans-serif" }}
+          className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white font-poppins"
+          style={{ fontFamily: "Poppins, sans-serif" }}
         >
           Welcome to the castle <span className="text-green-500">Delsgade</span>
         </h1>
